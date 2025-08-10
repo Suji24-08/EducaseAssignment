ab4107463db20adc5ee83aee21026a879fc6b705
Sujitha V <132780827+Suji24-08@users.noreply.github.com>
2025-08-10 21:42:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -96,8 +96,9 @@ app.get(
     }
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 
 });
+
